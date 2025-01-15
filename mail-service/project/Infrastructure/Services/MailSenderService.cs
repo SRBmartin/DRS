5dae77243f2600e64d4060797471f9bc8052f759
SRBMartin <makimuh02@gmail.com>
2025-01-15 22:16:43 +0100

    Enabled html type mails.

--- mail-service/project/Infrastructure/Services/MailSenderService.cs
+++ mail-service/project/Infrastructure/Services/MailSenderService.cs
@@ -22,7 +22,7 @@ public class MailSenderService : IMailSenderService
             message.From.Add(new MailboxAddress(_settings.SenderName, _settings.SenderEmail));
             message.To.Add(new MailboxAddress("", to));
             message.Subject = subject;
-            message.Body = new TextPart("plain") { Text = body };
+            message.Body = new TextPart("html") { Text = body };
 
             using var client = new SmtpClient();
             
