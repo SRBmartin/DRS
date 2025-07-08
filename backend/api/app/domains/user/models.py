cafa64daf9b0cf459b94fb0ca8499685bbcdb14b
Marko Kliska <93406686+MarkoKliska@users.noreply.github.com>
2025-07-08 22:53:01 +0200

    [SP]: Staging into main

--- backend/api/app/domains/user/models.py
+++ backend/api/app/domains/user/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from datetime import datetime, timedelta
 from ...core.extensions import db
+from ...infrastructure.utils.time import now
 import bcrypt
 
 class User(db.Model):
@@ -40,8 +41,8 @@ class Session(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     user_id = db.Column(UUID(as_uuid=True), ForeignKey('users.users.id'), nullable=False)
-    started_time = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    ending_time = db.Column(db.DateTime, default=lambda: datetime.now() + timedelta(hours=2), nullable=True)
+    started_time = db.Column(db.DateTime(timezone=True), default=lambda: now(), nullable=False)
+    ending_time = db.Column(db.DateTime(timezone=True), default=lambda: now() + timedelta(hours=2), nullable=False)
     ip_address = db.Column(db.String(45), nullable=False)
     logged_out = db.Column(db.Boolean, default=False, nullable=False)
 
